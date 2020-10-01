85292114db9a80b07fdf889d4c98154ec23ecbd8
priyanka205 <63328436+priyanka205@users.noreply.github.com>
2020-10-01 17:42:41 -0400

    added

---
+++ pythongithub/updatedpythonveryeasy/qa6.py
@@ -0,0 +1,9 @@
+#  Write a Python program to calculate the sum of three given numbers, 
+#  if the values are equal then return three times of their sum.
+
+
+x = int(input("Enter number 1: "))
+y = int(input("Enter number 2: "))
+z = int(input("Enter number 3: "))
+sum = x+y+z
+print(sum)
---
+++ pythongithub/updatedpythonveryeasy/qa7.py
@@ -0,0 +1,8 @@
+# 7) Write a Python program to concatenate all elements in a list into a string and return it.
+
+my_fruits = ["berry","orange","peach","avocado"]
+list = " ".join(my_fruits)
+print(list)
+
+ 
+
