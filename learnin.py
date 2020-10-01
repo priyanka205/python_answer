1cb1cc73a6abf614300266d670fef7521a29732e
priyanka205 <63328436+priyanka205@users.noreply.github.com>
2020-10-01 14:06:54 -0400

    added

--- learnin.py
+++ learnin.py
@@ -484,7 +484,7 @@ for f in fruits[1]:
 #print(a+b)
 
 
-# num = [10,12,15,46,45]
+# num = [30,20,10,12,15,46,45]
 # minimum = 100000000
 # for i in num:
 #     if minimum>i:
@@ -537,18 +537,23 @@ for f in fruits[1]:
 
 # # Prime number:
 
-lower_limit = int(input("Enter lower limit: "))
-upper_limit = int(input("Enter upper limit: "))
-prime_numbers = []
-for num in range(lower_limit,upper_limit+1):
-    if num>1:#if we need to test with negative number
-        for i in range(2,num):
-            if (num%i)==0:
-                break
-        else:
-            prime_numbers.append(num)
+# lower_limit = int(input("Enter lower limit: "))
+# upper_limit = int(input("Enter upper limit: "))
+# prime_numbers = []
+# for num in range(lower_limit,upper_limit+1):
+#     if num>1:#if we need to test with negative number
+#         for i in range(2,num):
+#             if (num%i)==0:
+#                 break
+#         else:
+#             prime_numbers.append(num)
 
-print(prime_numbers)
+# print(prime_numbers)
+
+# Fibonacci Numbers:
+
+
+
 
 
 
