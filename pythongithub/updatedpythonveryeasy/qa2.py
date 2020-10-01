c931060ab76e6b03c05fa835c5703d9289366d0c
priyanka205 <63328436+priyanka205@users.noreply.github.com>
2020-10-01 16:34:52 -0400

    answeradded

--- pythongithub/updatedpythonveryeasy/qa2.py
+++ pythongithub/updatedpythonveryeasy/qa2.py
@@ -2,3 +2,8 @@
 #  last name and print them in reverse order 
 # with a space between them.
 
+first_name = "Priyanka"
+last_name = "shrestha"
+from_opposite = last_name + " " + first_name
+print(from_opposite)
+
