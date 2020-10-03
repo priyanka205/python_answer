7bd4d62b2746470f1613a387557748782a48d2a3
priyanka205 <63328436+priyanka205@users.noreply.github.com>
2020-10-02 23:45:00 -0400

    try_except

---
+++ pythontopic/try_except.py
@@ -0,0 +1,26 @@
+# try and except:
+# these two comes together:
+
+# Lets try with the example:
+
+divider = [5,4,9,78,0,12]
+num =5550
+try:
+    for i in divider:
+        ans = num/i
+        print(ans)
+except:
+    print("Something went wrong")
+    
+
+
+
+
+
+
+
+
+
+
+
+
