17cf2c43cebc77410080ced13c729b4268e50ca3
priyanka205 <63328436+priyanka205@users.noreply.github.com>
2020-10-03 00:07:53 -0400

    added

--- pythontopic/try_except.py
+++ pythontopic/try_except.py
@@ -3,7 +3,7 @@
 
 # Lets try with the example:
 
-divider = [5,4,9,78,0,12]
+divider = [5,0,9,78,0,12]
 num =5550
 try:
     for i in divider:
@@ -11,6 +11,37 @@ try:
         print(ans)
 except:
     print("Something went wrong")
+
+# we can put except  multiple times like else
+
+divider = [5,6,9,78,0,12]
+num =5550
+try:
+    for i in divider:
+        ans = num/i
+        print(ans)
+except ZeroDivisionError:
+    print("Number cannot be divide by zero.")
+except:
+    print("Spmething went wrong.")
+
+#we can use here finally, finally:
+
+
+divider = [5,6,9,78,0,12]
+num =5550
+try:
+    for i in divider:
+        ans = num/i
+        print(ans)
+except ZeroDivisionError:
+    print("Number cannot be divide by zero.")
+except:
+    print("Spmething went wrong.")
+finally:
+    print("programming is completed")# this will print anyhow 
+
+
     
 
 
