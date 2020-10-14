8b87aa95822d1840eb63d7f3a55c4f680cea6d61
priyanka205 <63328436+priyanka205@users.noreply.github.com>
2020-10-14 11:45:20 -0400

    answer

---
+++ homework/pythonbasic1/qa5.py
@@ -0,0 +1,4 @@
+
+
+5) Write a Python program to get the difference between a given number and 17, if the number 
+is greater than 17 return double the absolute difference.
---
+++ homework/pythonbasic1/qa6.py
@@ -0,0 +1,7 @@
+# 6) Write a Python program to count the number 4 in a given list.
+
+# [5,4,8,9,4,66,77,11,4,65,147,6565,4,55,4]
+
+my_list = [5,4,8,9,4,66,77,11,4,65,147,6565,4,55,4]
+x = my_list.count(4)
+print(x)
