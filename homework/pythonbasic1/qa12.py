28545a129c544cf62cb6e0140db5b6f1295395f9
priyanka205 <63328436+priyanka205@users.noreply.github.com>
2020-10-14 22:55:07 -0400

    answer added

---
+++ homework/pythonbasic1/qa12.py
@@ -0,0 +1,9 @@
+# 2) Write a Python program to check if variable is of integer or string.
+
+# to check int:
+my_number = [8,5,8,6,2,4,6,2]
+print(isinstance(8,int))
+
+#to check string:"
+my_letter = ("priyanla","priya","Tisa")
+print(isinstance("Priyanka",str))
