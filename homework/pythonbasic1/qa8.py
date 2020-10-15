2fbe9bf278620283120fdf5dcf7af586bdd41f78
priyanka205 <63328436+priyanka205@users.noreply.github.com>
2020-10-14 21:03:21 -0400

    ask

---
+++ homework/pythonbasic1/qa8.py
@@ -0,0 +1,7 @@
+# Write a Python program to remove the first item from a specified list.
+my_item = ["Orange","Banana","Apple","peach"]
+
+print("my_flist: " ,my_item)
+
+del my_item[0]
+print(my_item)
