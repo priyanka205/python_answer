e2a35bbb6150a16ebf61e46945692f2355ce6e50
priyanka205 <63328436+priyanka205@users.noreply.github.com>
2020-10-14 20:51:20 -0400

    answer

---
+++ homework/pythonbasic1/qa7.py
@@ -0,0 +1,28 @@
+# ) Write a Python program to calculate the sum of three given numbers, 
+# if the values are equal then return three times of their sum.
+
+
+
+
+my_list1 = int(input("Enter a number: "))
+
+my_list2 = int(input("Enter a number: "))
+
+my_list3 = int(input("Enter a number: "))
+
+my_list4 = my_list1+my_list2+my_list3
+
+if my_list1 == my_list2 == my_list3:
+    my_list4 = my_list4 * 3
+
+print(my_list4)
+
+
+
+
+
+
+
+
+
+
