5633b4ea43c7f5c5694d7226941a5b265f49303b
priyanka205 <63328436+priyanka205@users.noreply.github.com>
2020-10-15 12:17:54 -0400

    added

--- homework/pythonbasic1/qa18.py
+++ homework/pythonbasic1/qa18.py
@@ -1,4 +1,10 @@
-18) Given a range of numbers. Iterate from 0th 
-number to the end number and print the sum of the current number and previous number.
+# 18) Given a range of numbers. Iterate from 0th 
+# number to the end number and print the sum of the current number and previous number.
 
-[88,55,92,4,45,25,31,97,85]
+# [88,55,92,4,45,25,31,97,85]
+
+my_numbers = [88,55,92,4,45,25,31,97,85]
+for i in  (my_numbers+1):
+    print(i + my_numbers)
+
+
