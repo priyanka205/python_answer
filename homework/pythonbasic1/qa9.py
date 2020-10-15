ab97fb9ce899c8e37159a5fb868f993f6bd743a3
priyanka205 <63328436+priyanka205@users.noreply.github.com>
2020-10-14 21:51:27 -0400

    answer

---
+++ homework/pythonbasic1/qa9.py
@@ -0,0 +1,14 @@
+
+#Write a Python program to input a number, if it is not a number generate an error message.
+ 
+my_number = int(input("Enter a number: "))
+print(my_number)
+breakpoint
+if my_number is  not my_number:
+    print("please enter a number")
+
+
+ 
+
+
+ 
