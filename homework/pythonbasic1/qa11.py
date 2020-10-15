eb7d72d2e948d0209b6a08d7ef31f63ad29d6aa6
priyanka205 <63328436+priyanka205@users.noreply.github.com>
2020-10-14 22:43:27 -0400

    added

---
+++ homework/pythonbasic1/qa11.py
@@ -0,0 +1,8 @@
+
+# 11) Write a Python program to check if multiple variables have the same value.
+my_list1 = (1,2,3,4,5,6,7,8,9)
+my_list2 = (1,2,3,4,5,6,7,8,9)
+if my_list1 == my_list2:
+    print("Both variables have same value")
+
+
