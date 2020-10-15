156f569273d111528505eef8c487ba9e7a9d0e08
priyanka205 <63328436+priyanka205@users.noreply.github.com>
2020-10-15 11:48:46 -0400

    added

--- homework/pythonbasic1/qa23.py
+++ homework/pythonbasic1/qa23.py
@@ -1,3 +1,6 @@
-23) Return the number of times that the string “Ram” appears anywhere in the given string.
+# 23) Return the number of times that the string “Ram” appears anywhere in the given string.
+# Given string is “Ram is a good developer. Ram is also a CAD Designer” and output should be 2.
 
-Given string is “Ram is a good developer. Ram is also a CAD Designer” and output should be 2.
+x = ("Ram is a good developer. Ram is also a CAD Designer")
+print(x.count("Ram"))
+
