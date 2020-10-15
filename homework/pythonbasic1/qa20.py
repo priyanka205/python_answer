b4c7a5001632e9a0e6aee1923242b11fbc355c2b
priyanka205 <63328436+priyanka205@users.noreply.github.com>
2020-10-15 15:45:12 -0400

    added

--- homework/pythonbasic1/qa20.py
+++ homework/pythonbasic1/qa20.py
@@ -1,8 +1,8 @@
 
 
-20) Given a string and an int n, remove characters from string starting
- from zero up to n and return a new string.
+# 20) Given a string and an int n, remove characters from string starting
+#  from zero up to n and return a new string.
 
-Example InputString: "HelloWorld", InputNumber: 5
+# Example InputString: "HelloWorld", InputNumber: 5
 
-Example OutputString: "World"
+# Example OutputString: "World"
---
+++ homework/pythonbasic2/qa1.py
@@ -0,0 +1,1 @@
+1) Print the multiplication table from 2 to 5, each containing up to 5 times of each numbers.
