61e6e6c6e60b01b7f0c0068a2ddb59390881fb44
priyanka205 <63328436+priyanka205@users.noreply.github.com>
2020-10-20 00:02:29 -0400

    anser

---
+++ homework/pythonbasic3/qa1.py
@@ -0,0 +1,14 @@
+
+# Write a function that returns the maximum of two numbers.
+
+my_list1 = 5
+my_list2 = 55
+def maximum(my_list1, my_list2): 
+    #   global
+    if my_list1 >= my_list2: 
+        return my_list1 
+    else: 
+        return my_list2
+      
+
+print(maximum(my_list1, my_list2)) 
