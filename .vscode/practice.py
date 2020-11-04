6f32f460d5d98bc96773d19f50705274bbeea20b
priyanka205 <63328436+priyanka205@users.noreply.github.com>
2020-11-04 12:20:20 -0500

    added

---
+++ .vscode/practice.py
@@ -0,0 +1,240 @@
+#  24) Calculate mutiplication of two random float numbers
+# Note:
+# First random float number must be between 0.1 and 1
+
+# Second random float number must be between 9.5 and 99.5
+
+# import random
+# my_fname1  = random.uniform(0.1, 1)
+# print(my_fname1)
+# my_fname2 = random.uniform(9.5, 99.5)
+# print(my_fname2)
+
+# multi = my_fname1 * my_fname2
+# print(multi)
+
+
+# import random
+# my_float = random.uniform(0.1,1)
+# print(my_float)
+# myfloat2 = random.uniform(9.5,99.5)
+# print(myfloat2)
+# final_float = my_float + myfloat2
+# print(final_float)
+
+
+
+# Generate  random String of length 5
+
+# Note: String must be the combination of the UPPER case and lower case letters only. 
+# No numbers and a special symbol.
+
+# import random
+# import string
+# random_letters = random.choices(string.ascii_uppercase+string.ascii_lowercase, k=5)
+# final_string = "".join(random_letters)
+# print(final_string)
+
+#  Iterate a given list and Check if a given element already exists in a dictionary as
+#   a key’s value if not delete it from the list
+# Given:
+# rollNumber = [47, 64, 69, 37, 76, 83, 95, 97]
+# sampleDict ={'Jhon':47, 'Emma':69, 'Kelly':76, 'Jason':97}
+# Expected Outcome:after removing unwanted elemnts from list [47, 69, 76, 97]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+
+     
+         
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
