3fdc4769532cd37a23ec7be9948f344c6f8f49e3
priyanka205 <63328436+priyanka205@users.noreply.github.com>
2020-11-12 09:58:38 -0500

    answer

---
+++ homework/w3functionpractice/qa6.py
@@ -0,0 +1,9 @@
+# 6. Write a Python function to check whether a number is in a given range. Go to the editor
+# Click me to see the sample solution-
+
+def test_range(n):
+    if n in range(3,9):
+        print( " %s is in the range"%str(n))
+    else :
+        print("The number is outside the given range.")
+test_range(7)
