eca92dec05dd19e961645c17faba2659c86ed5aa
priyanka205 <63328436+priyanka205@users.noreply.github.com>
2020-11-25 19:19:36 -0500

    added

--- webscrapping_qoutes/progwebscrapping.py
+++ webscrapping_qoutes/progwebscrapping.py
@@ -10,4 +10,25 @@ soup = BeautifulSoup(url_data.content, 'lxml')
 
 print(soup.title)
 
-print(soup.find("class"='wikitable sortable'))
+table = soup.find("table", class_ ='wikitable sortable')
+# table = soup.find_all("table",class_ = 'weikitable sortable')[1] if we need first table from entier website
+# print(table)
+
+# headers = table.find("thead/tr")
+headers = table.find_all("th")
+print(headers)
+
+final_headers = []
+for h in headers:
+    final_headers.append(h.text.strip())
+
+# print(final_headers)
+table_rows = table.find_all('tr')
+for tr in table_rows:
+    table_data = tr.find_all("td")
+    for td in table_data:
+        print(td.text)
+
+
+
+
